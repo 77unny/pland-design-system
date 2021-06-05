e9bb69ff6d051373d8b107b5ceb63396c4cdb6d8
baekCode <baekcode@gmail.com>
2021-06-06 02:06:28 +0900

    modify : bable-loader 수정 및 style 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Button from './components/Button';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Button children={'Enter'}/>
 
-
-    </div>
+    </>
   );
 }
 
